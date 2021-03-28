bd7c5142818d7fa5bbc0d52409c21da6a51dc11e
mzaink <sidsmith9@gmail.com>
2021-03-29 04:01:08 +0530

    Update record_widget.dart

--- lib/views/chat_view/widgets/record_widget.dart
+++ lib/views/chat_view/widgets/record_widget.dart
@@ -3,7 +3,14 @@ import 'package:provider/provider.dart';
 import 'package:whats_app/controllers/chat_view_controllers/audio_recording_controller.dart';
 import 'package:whats_app/controllers/chat_view_controllers/chat_controller.dart';
 
-class RecordWidget extends StatelessWidget {
+class RecordWidget extends StatefulWidget {
+  @override
+  _RecordWidgetState createState() => _RecordWidgetState();
+}
+
+class _RecordWidgetState extends State<RecordWidget> {
+  double width = 400;
+
   @override
   Widget build(BuildContext context) => ChangeNotifierProvider<AudioRecordingController>(
         create: (context) => AudioRecordingController(
@@ -13,19 +20,209 @@ class RecordWidget extends StatelessWidget {
           ),
         ),
         child: Consumer<AudioRecordingController>(
-          builder: (context, controller, child) => RaisedButton(
-            child: Text(
-              controller.currentState.isRecording ? 'Stop Recording' : 'Start Recording',
-            ),
-            onPressed: () {
-              print('Recording state ${controller.currentState.isRecording}');
-              if (controller.currentState.isRecording) {
-                controller.stopRecording();
-              } else {
-                controller.startRecording();
-              }
-            },
+          builder: (context, controller, child) => Row(
+            children: [
+              Flexible(
+                child: Container(
+                  width: width,
+                  padding: EdgeInsets.all(8.0),
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      controller.currentState.isRecording
+                          ? Expanded(child: _CancelSliderWidgetForRecordingButton())
+                          : Expanded(child: _TipsWidgetForRecordingButton()),
+                      SizedBox(width: 5.0),
+                      AudioRecordingButton(
+                        onLongPressMoveUpdate: (offset) {
+                          updateUIOrStopRecordingBasedOn(offset, controller: controller);
+                        },
+                        onLongPressUp: () {
+                          resetUIToOriginalState();
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       );
+
+  void updateUIOrStopRecordingBasedOn(
+    Offset dragOffset, {
+    AudioRecordingController controller,
+  }) {
+    final distanceDraggedFromRight = -dragOffset.dx;
+
+    if (distanceDraggedFromRight > 120.0) {
+      controller.cancelRecording();
+      setState(() {
+        width = double.infinity;
+      });
+    } else {
+      setState(() {
+        width = MediaQuery.of(context).size.width - distanceDraggedFromRight;
+      });
+    }
+  }
+
+  void resetUIToOriginalState() {
+    setState(() {
+      width = double.infinity;
+    });
+  }
+}
+
+class _CancelSliderWidgetForRecordingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Consumer<AudioRecordingController>(
+        builder: (context, controller, child) => Container(
+          height: 40.0,
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.blueGrey.withAlpha(120),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text('${controller.currentState.secondsElapsedSinceRecordingStarted.toSecondsAndMinutes}'),
+              Row(
+                children: [
+                  Text(
+                    'Slide to cancel',
+                    softWrap: true,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  VerticalDivider(
+                    color: Colors.grey,
+                    indent: 10.0,
+                    endIndent: 10.0,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class _TipsWidgetForRecordingButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Consumer<AudioRecordingController>(
+        builder: (context, controller, child) => Container(
+          height: 40.0,
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: Colors.blueGrey.withAlpha(120),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: TextField(
+                  decoration: InputDecoration(
+                    hintText: 'Type a message',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  VerticalDivider(
+                    color: Colors.grey,
+                    indent: 10.0,
+                    endIndent: 10.0,
+                  ),
+                  Icon(Icons.chevron_right),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+}
+
+class AudioRecordingButton extends StatelessWidget {
+  static const double disabledRadius = 20.0;
+  static const double enabledRadius = 25.0;
+
+  final void Function(Offset offset) onLongPressMoveUpdate;
+  final VoidCallback onLongPressUp;
+
+  const AudioRecordingButton({
+    Key key,
+    this.onLongPressMoveUpdate,
+    this.onLongPressUp,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Consumer<AudioRecordingController>(
+        builder: (context, controller, child) => Transform.scale(
+          scale: controller.currentState.isRecording ? 1.7 : 1.0,
+          child: GestureDetector(
+            onLongPress: () {
+              controller.startRecording();
+            },
+            onLongPressUp: () {
+              controller.stopRecording();
+              onLongPressUp?.call();
+            },
+            onLongPressMoveUpdate: (longPressMoveUpdate) {
+              onLongPressMoveUpdate?.call(longPressMoveUpdate.localOffsetFromOrigin);
+            },
+            child: AnimatedContainer(
+              duration: Duration(milliseconds: 700),
+              curve: Curves.bounceOut,
+              height: radiusBasedOnCurrentState(controller) * 2,
+              width: radiusBasedOnCurrentState(controller) * 2,
+              decoration: BoxDecoration(
+                color: Colors.teal,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.mic,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+
+  double radiusBasedOnCurrentState(AudioRecordingController controller) {
+    if (controller.currentState.isRecording) {
+      return AudioRecordingButton.enabledRadius;
+    } else {
+      return AudioRecordingButton.disabledRadius;
+    }
+  }
+}
+
+extension on int {
+  String get toSecondsAndMinutes {
+    final numberOfMinutes = this ~/ 60;
+    final numberOfSeconds = this % 60;
+
+    if (numberOfMinutes < 10) {
+      if (numberOfSeconds < 10) {
+        return '0$numberOfMinutes:0$numberOfSeconds'; //
+      } else {
+        return '0$numberOfMinutes:$numberOfSeconds'; //
+      }
+    } else {
+      if (numberOfSeconds < 10) {
+        return '$numberOfMinutes:0$numberOfSeconds'; //
+      } else {
+        return '$numberOfMinutes:$numberOfSeconds'; //
+      }
+    }
+  }
 }

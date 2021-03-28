eba8276f6b7f00946d8a9f594ef0f593179791c6
mzaink <sidsmith9@gmail.com>
2021-03-29 00:31:32 +0530

    Create chat_widget.dart

---
+++ lib/views/chat_view/widgets/chat_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:whats_app/controllers/chat_view_controllers/chat_controller.dart';
+import 'package:whats_app/views/chat_view/widgets/audio_message_widget.dart';
+
+class ChatWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Consumer<ChatController>(
+        builder: (context, controller, message) => ListView.builder(
+          itemBuilder: (context, index) {
+            if (controller.currentState.messages.isEmpty) {
+              return _buildNoMessagesWidget(
+                context,
+                controller: controller,
+              );
+            } else {
+              return _buildChatWidget(
+                context,
+                index: index,
+                controller: controller,
+              );
+            }
+          },
+          itemCount: controller.currentState.messages.length,
+        ),
+      );
+
+  Widget _buildNoMessagesWidget(BuildContext context, {@required ChatController controller}) => Center(
+        child: Text('Nothing to show here!'),
+      );
+
+  Widget _buildChatWidget(
+    BuildContext context, {
+    @required int index,
+    @required ChatController controller,
+  }) =>
+      AudioMessageWidget(
+        recordedAudioMessage: controller.currentState.messages[index],
+      );
+}

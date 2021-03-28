5be994ae452a07f458a0ec450029fc8f15224c78
mzaink <sidsmith9@gmail.com>
2021-03-29 00:30:12 +0530

    Rename record_widget_controller.dart to audio_recording_controller.dart

--- lib/controllers/chat_view_controllers/audio_recording_controller.dart
+++ lib/controllers/chat_view_controllers/audio_recording_controller.dart
@@ -1,34 +1,37 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:whats_app/controllers/chat_view_controllers/chat_widget_controller.dart';
+import 'package:whats_app/controllers/chat_view_controllers/chat_controller.dart';
 import 'package:whats_app/models/messages/recorded_audio_message.dart';
 import 'package:whats_app/utils/audio_utils/audio_recorder.dart';
 import 'package:whats_app/utils/audio_utils/track_details.dart';
+import 'package:whats_app/utils/permission_utils/permission_utils.dart';
 
 class RecordState {
   final bool isRecording;
   final int secondsElapsedSinceRecordingStarted;
 
-  RecordState(
-      {this.isRecording = false, this.secondsElapsedSinceRecordingStarted = 0})
+  RecordState({this.isRecording = false, this.secondsElapsedSinceRecordingStarted = 0})
       : assert(isRecording != null, 'isRecording cannot be null'),
-        assert(secondsElapsedSinceRecordingStarted != null,
-            'secondsElapsedSinceRecordingStarted cannot be null');
+        assert(secondsElapsedSinceRecordingStarted != null, 'secondsElapsedSinceRecordingStarted cannot be null');
 
-  RecordState copyWith(
-      {bool isRecording = false, int secondsElapsedSinceRecordingStarted}) {
+  RecordState copyWith({
+    bool isRecording,
+    int secondsElapsedSinceRecordingStarted,
+  }) {
     return RecordState(
       isRecording: isRecording ?? this.isRecording,
       secondsElapsedSinceRecordingStarted:
-          secondsElapsedSinceRecordingStarted ??
-              this.secondsElapsedSinceRecordingStarted,
+          secondsElapsedSinceRecordingStarted ?? this.secondsElapsedSinceRecordingStarted,
     );
   }
 }
 
-abstract class RecordWidgetController extends ChangeNotifier {
+abstract class AudioRecordingController extends ChangeNotifier {
   RecordState get currentState;
+
+  factory AudioRecordingController({@required ChatController chatController}) =>
+      _AudioRecordingControllerImpl(chatController: chatController);
 
   void startRecording();
 
@@ -37,36 +40,35 @@ abstract class RecordWidgetController extends ChangeNotifier {
   void cancelRecording();
 }
 
-class _RecordWidgetControllerImpl extends ChangeNotifier
-    implements RecordWidgetController {
+class _AudioRecordingControllerImpl extends ChangeNotifier implements AudioRecordingController {
   AudioRecorder _audioRecorder;
-  ChatWidgetController _chatWidgetController;
+  ChatController _chatController;
 
   @override
   RecordState currentState = RecordState();
 
-  _RecordWidgetControllerImpl(
-      {@required ChatWidgetController chatWidgetController})
-      : assert(chatWidgetController != null,
-            'chatWidgetController cannot be null'),
+  _AudioRecordingControllerImpl({@required ChatController chatController})
+      : assert(chatController != null, 'chatWidgetController cannot be null'),
         this.currentState = RecordState(),
         this._audioRecorder = AudioRecorder(),
-        this._chatWidgetController = chatWidgetController;
+        this._chatController = chatController;
 
   TrackDetails _temporaryTrackDetails;
   Timer _recordingDurationTimer;
 
   @override
   void startRecording() async {
-    _temporaryTrackDetails = await _audioRecorder.startRecording();
+    final isMicrophonePermissionGranted = await requestMicrophonePermission();
 
-    final isRecording = _temporaryTrackDetails != null;
+    if (isMicrophonePermissionGranted) {
+      _temporaryTrackDetails = await _audioRecorder.startRecording();
 
-    if (isRecording) {
-      _updateState(currentState.copyWith(isRecording: isRecording));
-      _createTimerToTrackRecordingDuration();
-    } else {
-      _updateState(currentState.copyWith(isRecording: isRecording));
+      final isRecording = _temporaryTrackDetails != null;
+
+      if (isRecording) {
+        _updateState(currentState.copyWith(isRecording: true));
+        _createTimerToTrackRecordingDuration();
+      }
     }
   }
 
@@ -106,13 +108,14 @@ class _RecordWidgetControllerImpl extends ChangeNotifier
       Duration(seconds: 1),
       (timer) {
         if (currentState.isRecording) {
+          print('isRecording = true');
           final updatedState = currentState.copyWith(
-            secondsElapsedSinceRecordingStarted:
-                currentState.secondsElapsedSinceRecordingStarted + 1,
+            secondsElapsedSinceRecordingStarted: currentState.secondsElapsedSinceRecordingStarted + 1,
           );
 
           _updateState(updatedState);
         } else {
+          print('isRecording = false');
           final updatedState = currentState.copyWith(
             secondsElapsedSinceRecordingStarted: 0,
           );
@@ -125,10 +128,9 @@ class _RecordWidgetControllerImpl extends ChangeNotifier
   }
 
   void _addRecordedMessageToChat() {
-    final recordedAudioMessage =
-        RecordedAudioMessage(trackDetails: _temporaryTrackDetails);
+    final recordedAudioMessage = RecordedAudioMessage(trackDetails: _temporaryTrackDetails);
 
-    _chatWidgetController.addMessage(recordedAudioMessage);
+    _chatController.addMessage(recordedAudioMessage);
   }
 
   void _clearTemporaryTrackDetails() {
